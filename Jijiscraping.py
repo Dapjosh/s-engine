cd613995e7fa12829657927d6f94d54600ccd2d4
Oladapo Siyanbola <siyanbolaoladapo@gmail.com>
2024-04-16 15:07:15 +0100

    Update Jijiscraping.py

--- Jijiscraping.py
+++ Jijiscraping.py
@@ -11,8 +11,8 @@ import json
 import time
 import csv
 import os
-# options = webdriver.ChromeOptions()
-options = Options()
+options = webdriver.ChromeOptions()
+# options = Options()
 
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
 
